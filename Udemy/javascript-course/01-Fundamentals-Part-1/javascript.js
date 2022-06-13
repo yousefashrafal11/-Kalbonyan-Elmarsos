0b64c6f7539bc8c97b21074080f942126d703740
yousefashrafal11 <84685577+yousefashrafal11@users.noreply.github.com>
2022-06-13 22:12:18 +0200

    update

--- Udemy/javascript-course/01-Fundamentals-Part-1/javascript.js
+++ Udemy/javascript-course/01-Fundamentals-Part-1/javascript.js
@@ -461,3 +461,23 @@ console.log(`I'm ${2037 - 1991} years old ${me}`);
 
 ////////////////////////////////////
 // The Conditional (Ternary) Operator
+
+const age = 23;
+// age >= 18
+//   ? console.log("I like to drink tea")
+//   : console.log("I like to drink water ");
+
+const drink = age >= 18 ? "tea" : "water";
+console.log(drink);
+
+
+let drink2;
+if (age >= 18) {
+  drink2 = "tea";
+} else {
+  drink2 = "water";
+}
+
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? 'tea 😥' : 'water 💧'}`);
