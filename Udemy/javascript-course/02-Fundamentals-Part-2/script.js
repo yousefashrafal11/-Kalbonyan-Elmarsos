9d8040999fa2bf87ab5fc86f9628f98f0572e1d2
yousefashrafal11 <yousefashrafal@gmail.com>
2022-06-20 18:12:16 +0200

    update

---
+++ Udemy/javascript-course/02-Fundamentals-Part-2/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+// here we are change variable name so not working
+// if (passTest) hasDriverLicense = true;
+
+if (passTest) hasDriversLicense = true;
+
+if (hasDriversLicense) console.log("I Can Drive :D");
