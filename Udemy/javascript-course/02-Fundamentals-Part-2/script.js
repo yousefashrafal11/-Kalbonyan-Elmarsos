45de37d83d611469f2a9a1b76834e9cf559dc0db
yousefashrafal11 <84685577+yousefashrafal11@users.noreply.github.com>
2022-06-26 08:00:19 +0200

    update

--- Udemy/javascript-course/02-Fundamentals-Part-2/script.js
+++ Udemy/javascript-course/02-Fundamentals-Part-2/script.js
@@ -89,6 +89,7 @@ const yearsUntil = yearsUntilRetirement(1991);
 console.log(yearsUntil);
 */
 
+/*
 function cutFuritPieces(fruit) {
   return fruit * 4;
 }
@@ -101,3 +102,91 @@ function fruitProcesor(apples, oranges) {
   return juice;
 }
 console.log(fruitProcesor(2, 3));
+*/
+
+/*
+const calcAge = function(birthYear){
+  return    2037 - birthYear;
+}
+
+const yearsUntilRetirement = function(birthYear) {
+  const age = calcAge(birthYear);
+  const retirement = 65 - age;
+  return retirement;
+};
+
+console.log(yearsUntilRetirement(1992, "Youssef"));
+console.log(yearsUntilRetirement(1965, "Ashraf"));
+*/
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/*
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
+Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
+A team ONLY wins if it has at least DOUBLE the average score of the other team. Otherwise, no team wins!
+1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+2. Use the function to calculate the average for both teams
+3. Create a function 'checkWinner' that takes the average score of each team as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner to the console, together with the victory points, according to the rule above. Example: "Koalas win (30 vs. 13)".
+4. Use the 'checkWinner' function to determine the winner for both DATA 1 and DATA 2.
+5. Ignore draws this time.
+TEST DATA 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+TEST DATA 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+HINT: To calculate average of 3 values, add them all together and divide by 3
+HINT: To check if number A is at least double number B, check for A >= 2 * B. Apply this to the team's average scores 😉
+GOOD LUCK 😀
+*/
+
+
+/*
+const calcAverage  = (a, b, c) => (a + b + c) / 3;
+console.log(calcAverage(3, 4, 5));
+
+// Test 1
+let scoreDolphins = calcAverage(44, 23, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
+console.log(scoreDolphins, scoreKoalas);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+  if (avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins win 🏆 (${avgDolphins} vs. ${avgKoalas})`);
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    console.log(`Koalas win 🏆 (${avgKoalas} vs. ${avgDolphins})`);
+  } else {
+    console.log('No team wins...');
+  }
+}
+
+checkWinner(scoreDolphins, scoreKoalas);
+checkWinner(576, 111);
+
+// Test 2
+scoreDolphins = calcAverage(85, 54, 41);
+scoreKoalas = calcAverage(23, 34, 27);
+console.log(scoreDolphins, scoreKoalas);
+checkWinner(scoreDolphins, scoreKoalas);
+*/
+
+///////////////////////////////////////
+// Introduction to Arrays
+
+const friend1 = "Ahmed";
+const friend2 = "Ali";
+const friend3 = "Sayed";
+const friend4 = "Osama";
+
+const friends = ['Ahmed', 'Ali', 'Sayed', 'Osama'];
+console.log(friends);
+
+const years = new Array(1991, 1984, 2008, 2020);
+// console.log(years);
+console.log(friends[0]);
+console.log(friends[2]);
+
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+
+
