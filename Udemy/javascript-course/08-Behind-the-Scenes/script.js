c49cf09709b3b663d2144c2c9c7602886bd13f6c
yousefashrafal11 <84685577+yousefashrafal11@users.noreply.github.com>
2022-07-25 02:40:58 +0200

    update

--- Udemy/javascript-course/08-Behind-the-Scenes/script.js
+++ Udemy/javascript-course/08-Behind-the-Scenes/script.js
@@ -1,36 +1,89 @@
 'use strict';
 
-function calcAge(birthYear) {
-  const age = 2037 - birthYear;
-  // console.log(firstName);
-  function printAge() {
-    const output = `${firstName}, You are ${age}, born in ${birthYear}`;
-    console.log(output);
+// function calcAge(birthYear) {
+//   const age = 2037 - birthYear;
+//   // console.log(firstName);
+//   function printAge() {
+//     const output = `${firstName}, You are ${age}, born in ${birthYear}`;
+//     console.log(output);
 
-    if (birthYear >= 1981 && birthYear <= 1996) {
-      var millenial = true;
-      // Creating NEW variable with same name as outer scope's variable
-      const firstName = 'Yassen';
-      // Reasssigning outer scope's variable
-      output = 'NEW OUTPUT';
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       var millenial = true;
+//       // Creating NEW variable with same name as outer scope's variable
+//       const firstName = 'Yassen';
+//       // Reasssigning outer scope's variable
+//       output = 'NEW OUTPUT';
 
-      const str = `Oh, and you're a millenial, ${firstName}`;
-      console.log(str);
+//       const str = `Oh, and you're a millenial, ${firstName}`;
+//       console.log(str);
 
-      function add(a, b) {
-        return a + b;
-      }
-    }
-    console.log(millenial);
-    // add(2, 3);
+//       function add(a, b) {
+//         return a + b;
+//       }
+//     }
+//     console.log(millenial);
+//     // add(2, 3);
 
-    console.log(output);
-  }
+//     console.log(output);
+//   }
 
-  printAge();
+//   printAge();
 
-  return age;
-}
+//   return age;
+// }
 
-const firstName = 'Youssef';
-calcAge(1992);
+// const firstName = 'Youssef';
+// calcAge(1992);
+
+// let x = 20,
+//   y = 10;
+
+//   let result = add(x, y);
+//   console.log(result);
+
+// function add(c, d) {
+//   return c + d;
+// }
+
+// console.log(me);
+// console.log(job);
+// console.log(year);
+
+// var me = 'Youssef';
+// let job = 'Developer';
+// const year = 1991;
+
+// Functions
+// console.log(addDecl(2, 3));
+// // console.log(addExpr(2, 3));
+// console.log(addArrow);
+// // console.log(addArrow(2, 3));
+
+// function addDecl(a, b) {
+//   return a + b;
+// }
+
+// const addExpr = function (a, b) {
+//   return a + b;
+// };
+
+// var addArrow = (a, b) => a + b;
+
+// // Example
+// console.log(undefined);
+// if (!numProducts) deleteShoppingCart();
+
+// var numProducts = 10;
+
+// function deleteShoppingCart() {
+//   console.log('All products deleted!');
+// }
+
+// var x = 1;
+// let y = 2;
+// const z = 3;
+
+// console.log(x === window.x);
+// console.log(y === window.y);
+// console.log(z === window.z);
+
