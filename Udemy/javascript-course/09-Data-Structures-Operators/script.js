ad534b81c6aaf45bf3f9354ce518e46f485dcb2c
yousefashrafal11 <84685577+yousefashrafal11@users.noreply.github.com>
2022-08-07 09:18:13 +0200

    update

--- Udemy/javascript-course/09-Data-Structures-Operators/script.js
+++ Udemy/javascript-course/09-Data-Structures-Operators/script.js
@@ -4,6 +4,11 @@
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
+  // 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
   const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
   
   // ES6 enhanced object literals
@@ -23,6 +28,123 @@ const flights =
     },
   };
 
+  const getCode = str => str.slice(0,3).toUpperCase();
+
+  for(const flight of flights.split('+')){
+    // console.log(flight);
+    const [type, from, to, time] = flight.split(';')
+    // console.log(type, from, to, time);
+    const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll('_', ' ')} ${getCode(from)} ${getCode(to)} (${time.replace(':', 'h')})`.padStart(36);
+    console.log(output);
+  }
+  
+  /* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+  /*
+  document.body.append(document.createElement('textarea'));
+  document.body.append(document.createElement('button'));
+  
+  document.querySelector('button').addEventListener('click', function () {
+    const text = document.querySelector('textarea').value;
+    // console.log(text);
+    const rows = text.split('\n');
+    // console.log(rows);
+    
+    for(const [i, row] of rows.entries()){
+      const [first, second] = row.toLowerCase().trim().split('_');
+      // console.log(first, second);
+      const  output = `${first}${second.replace(
+        second[0],
+        second[0].toUpperCase()
+      )} `;
+      console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+
+    }
+  });
+*/
+  /*
+  // Split and join
+console.log('a+very+nice+string'.split('+')); //The split() method takes a pattern and divides a String into an ordered list of substrings by searching for the pattern, puts these substrings into an array, and returns the array.
+console.log('Jonas Schmedtmann'.split(' '));
+
+const [firstName, lastName] = 'Youssef Ashraf'.split(' ');
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');//The join() method creates and returns a new string by concatenating all of the elements in an array (or an array-like object), separated by commas or a specified separator string. If the array has only one item, then that item will be returned without using the separator.
+console.log(newName);
+
+const capitalizeName = function (name){
+  const names = name.split(' ');
+  const namesUpper = [];
+
+  for(const n of names){
+
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+
+  }
+  console.log(namesUpper);
+}
+
+capitalizeName('jessica ann smith davis');
+capitalizeName('youssef ashraf');
+
+const message = "Go to gate 23!";
+//The padStart() method pads the current string with another string (multiple times, if needed) until the resulting string reaches the given length. The padding is applied from the start of the current string.
+console.log(message.padStart(25, '+' ).padEnd(35, '+'));
+console.log('Youssef'.padStart(25, '+' ))
+
+const maskCreditCard = function(number){
+  const str = number + '';
+  const last = str.slice(-4);
+
+  return last.padStart(str.length, '*');
+}
+
+console.log(maskCreditCard(861486486));
+console.log(maskCreditCard(5656489648941921));
+console.log(maskCreditCard('81468709849645604088464'));
+
+// Repeat
+const message2 = 'Bad waether... All Departues Delayed... ';
+console.log(message2.repeat(5));
+
+const planesInLine = function(n) {
+  console.log(`there are ${n} planes in line ${'✈'.repeat(n)}`);
+} 
+
+planesInLine(3);
+planesInLine(7);
+planesInLine(12);
+*/
+
+  /*
+  // Working With String - part 2
   const airline = 'TAP Air Portugal';
   console.log(airline.toLowerCase());
   console.log(airline.toUpperCase());
@@ -39,12 +161,13 @@ const flights =
   // comparing Emails
   const email = "hello@youssef.io";
   const loginEmail = '  Hello@Youssef.Io ';
-/*  console.log(loginEmail);
-  const lowerEmail = loginEmail.toLowerCase();
-  console.log(lowerEmail);
-  const trimedEmail = lowerEmail.trim(); //The trim() method removes whitespace from both ends of a string and returns a new string
-  console.log(trimedEmail);
-*/
+////////
+  // console.log(loginEmail);
+  // const lowerEmail = loginEmail.toLowerCase();
+  // console.log(lowerEmail);
+  // const trimedEmail = lowerEmail.trim(); //The trim() method removes whitespace from both ends of a string and returns a new string
+  // console.log(trimedEmail);
+//////////////
 const normalizedEmail = loginEmail.toLowerCase().trim();
 console.log(normalizedEmail);
   console.log(email === normalizedEmail);
@@ -83,7 +206,8 @@ console.log(normalizedEmail);
   checkBaggage('I have a laptop, some Food and a pocket Knife');
   checkBaggage('Socks and camera');
   checkBaggage('Got some snacks and a gun for protection');
-  
+ */
+
 /*
   ///////////////////////////////////////
 // Working With Strings - Part 1
