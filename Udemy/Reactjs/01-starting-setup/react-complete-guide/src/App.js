a86dc71c5e3bd973001a820e1a8aec3242c5ba3c
yousefashrafal11 <84685577+yousefashrafal11@users.noreply.github.com>
2022-09-04 08:20:30 +0200

    update

--- Udemy/Reactjs/01-starting-setup/react-complete-guide/src/App.js
+++ Udemy/Reactjs/01-starting-setup/react-complete-guide/src/App.js
@@ -1,16 +1,79 @@
 // import logo from "./logo.svg";
 import ExpenseItem from "./components/ExpenseItem";
 import "./App.css";
+///////////////////////////////
+// Outputting Dynamic Data & Working with Expressions in JSX
+/*
+function App() {
+  return (
+    <div className="App">
+    <div>
+    <h2>Let's get started!</h2>
+    <ExpenseItem></ExpenseItem>
+    <ExpenseItem></ExpenseItem>
+    <ExpenseItem></ExpenseItem>
+    <ExpenseItem></ExpenseItem>
+    </div>
+    </div>
+    );
+  }
+  */
+///////////////////////////////////
+////  Passing Data via "props"
 
 function App() {
+  const expense = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
   return (
     <div className="App">
       <div>
         <h2>Let's get started!</h2>
-        <ExpenseItem></ExpenseItem>
+        <ExpenseItem
+          id={expense[0].id}
+          title={expense[0].title}
+          amount={expense[0].amount}
+          date={expense[0].date}
+        />
+        <ExpenseItem
+          id={expense[1].id}
+          title={expense[1].title}
+          amount={expense[1].amount}
+          date={expense[1].date}
+        />
+        <ExpenseItem
+          id={expense[2].id}
+          title={expense[2].title}
+          amount={expense[2].amount}
+          date={expense[2].date}
+        />
+        <ExpenseItem
+          id={expense[3].id}
+          title={expense[3].title}
+          amount={expense[3].amount}
+          date={expense[3].date}
+        />
       </div>
     </div>
   );
 }
-
 export default App;
--- Udemy/Reactjs/01-starting-setup/react-complete-guide/src/components/ExpenseItem.js
+++ Udemy/Reactjs/01-starting-setup/react-complete-guide/src/components/ExpenseItem.js
@@ -1,11 +1,58 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
+/*
+/////////////////////////////////
+// Outputting Dynamic Data & Working with Expressions in JSX
+
 const ExpenseItem = function () {
+  const expenseDate = new Date(2022, 9, 3);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
+
   return (
     <div className="expense-item">
-      <div>March 28th 2022</div>
+    //   <div>March 28th 2022</div>
+    //   <div className="expense-item__description">
+    //     <h2>Car Insurance</h2>
+    //     <div className="expense-item__price">$294.67</div>
+    //   </div> 
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
+    </div>
+  );
+};
+
+*/
+
+///////////////////////////////////
+////  Passing Data via "props"
+/*
+const ExpenseItem = function (props) {
+  return (
+      <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+      <h2>{props.title}</h2>
+      <div className="expense-item__price">${props.amount}</div>
+      </div>
+      </div>
+      );
+    };
+    */
+
+///////////////////////////////////
+// Adding "normal" JavaScript Logic to Components
+
+const ExpenseItem = function (props) {
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
