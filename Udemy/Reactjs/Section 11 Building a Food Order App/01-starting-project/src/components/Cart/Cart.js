c504a8c8783babec837840003dd75eaaf0473544
yousefashrafal11 <yousefashrafal@gmail.com>
2022-09-19 15:44:51 +0200

    update

--- Udemy/Reactjs/Section 11 Building a Food Order App/01-starting-project/src/components/Cart/Cart.js
+++ Udemy/Reactjs/Section 11 Building a Food Order App/01-starting-project/src/components/Cart/Cart.js
@@ -1,14 +1,34 @@
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
+
   const carItems = (
     <ul className={classes["cart-items"]}>
-      {[
-        [{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
-          <il>{item.name}</il>
-        )),
-      ]}
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
+      ))}
     </ul>
   );
   return (
@@ -17,14 +37,14 @@ const Cart = (props) => {
       {carItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         {/* here we get data from app file element cart */}
         <button className={classes["button--alt"]} onClick={props.onClose}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
--- Udemy/Reactjs/Section 11 Building a Food Order App/01-starting-project/src/store/CartProvider.js
+++ Udemy/Reactjs/Section 11 Building a Food Order App/01-starting-project/src/store/CartProvider.js
@@ -11,12 +11,48 @@ const defaultCartState = {
 const cartReduce = (state, action) => {
   // make if condition on Reduce
   if (action.type === "ADD") {
-    const updateItems = state.items.concat(action.item);
     const updateTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
+
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
+
     return {
-      items: updateItems,
+      items: updatedItems,
       totalAmount: updateTotalAmount,
+    };
+  }
+  if (action.type === "REMOVE") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
     };
   }
   return defaultCartState;
